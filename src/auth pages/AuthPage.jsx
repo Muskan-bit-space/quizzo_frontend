0f7231f53e8fe38db581f1abf2db8dc78ba76719
Muskan <muskan4mann@gmail.com>
2026-01-25 18:53:47 +0530

    QUIZLY

--- src/auth pages/AuthPage.jsx
+++ src/auth pages/AuthPage.jsx
@@ -8,7 +8,7 @@ const AuthPage = () => {
   return (
     <div className="auth-container">
       <div className="auth-title">
-        <h1 className="glitch" data-text="QUIZZO">QUIZZO</h1>
+        <h1 className="glitch" data-text="QUIZZO">QUIZLY</h1>
       </div>
       <nav className="auth-nav">
         <button className="auth-btn pulse">
